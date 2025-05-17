5b43153bf17c21f0bc32c2bfbdfced54547a2ab4
Evangelos Xaniadakis <echaniadakis@gmail.com>
2025-05-17 10:42:01 +0300

    final report

--- report/compile.sh
+++ report/compile.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# compile LaTeX document and its bib references
 lualatex -output-directory=out -interaction=nonstopmode Report.tex
 biber --input-directory=out Report
 lualatex -output-directory=out -interaction=nonstopmode Report.tex
-#lualatex -interaction=nonstopmode Report.tex
+
+# if production mode, output final PDF in its place
+if [ "$1" == "prod" ]; then
+    lualatex -output-directory=out -interaction=nonstopmode Report.tex
+    cp out/Report.pdf ..
+    echo "Production PDF ready."
+fi
